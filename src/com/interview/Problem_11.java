90dc20ba73aa8218de4d121e3c6e3f7adea1c904
weiqianghu <hwq.ecust@gmail.com>
2017-07-04 23:14:00 +0800

    数值的整数次方

---
+++ src/com/interview/Problem_11.java
@@ -0,0 +1,69 @@
+package com.interview;
+
+/**
+ * Created by weiqianghu on 17-7-4.
+ */
+public class Problem_11 {
+
+    public static void main(String[] args) {
+        System.out.println(power(3.0, -2) + " " + powerPlus(3.0, -2));
+        System.out.println(power(2, 0) + " " + powerPlus(2, 0));
+        System.out.println(power(4, 2) + " " + powerPlus(4, 2));
+        System.out.println(power(0, 0) + " " + powerPlus(0, 0));
+        System.out.println(power(-4, 3) + " " + powerPlus(-4, 3));
+        System.out.println(power(-4, -2) + " " + powerPlus(-4, -2));
+    }
+
+    private static double power(double base, int exponent) {
+        boolean isNegative = false;
+
+        if (exponent < 0) {
+            isNegative = true;
+            exponent = -exponent;
+        } else if (exponent == 0) {
+            return 1;
+        } else if (base == 0) {
+            return 1;
+        }
+
+        double result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result = result * base;
+        }
+
+        return isNegative ? 1 / result : result;
+    }
+
+    private static double powerPlus(double base, int exponent) {
+        if (equalsDouble(base, 0.0)) {
+            return 0;
+        }
+        if (exponent > 0) {
+            return powerWithUnsignedExponent(base, exponent);
+        }
+        return 1 / powerWithUnsignedExponent(base, -exponent);
+    }
+
+    private static boolean equalsDouble(double num1, double num2) {
+        return (num1 - num2 > 0.0000000001) && (num2 - num1 > 0.00000001);
+    }
+
+
+    private static double powerWithUnsignedExponent(double base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent == 1) {
+            return base;
+        }
+
+        double result = powerWithUnsignedExponent(base, exponent >> 1);
+        result *= result;
+        if ((exponent & 0x1) == 1) {
+            result *= base;
+        }
+
+        return result;
+    }
+
+}
