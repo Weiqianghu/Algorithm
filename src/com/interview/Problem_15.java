6d7505f04fd5df9fd01158b2ce085b6fafb02ce4
weiqianghu <hwq.ecust@gmail.com>
2017-07-08 17:53:22 +0800

    链表的倒数 k 个结点，反转链表

---
+++ src/com/interview/Problem_15.java
@@ -0,0 +1,56 @@
+package com.interview;
+
+/**
+ * Created by weiqianghu on 17-7-8.
+ */
+public class Problem_15 {
+    public static void main(String[] args) {
+        Node head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, new Node(6, null))))));
+        printKNode(head, 3);
+        printKNode(head, 6);
+        printKNode(head, 7);
+        printKNode(head, 0);
+        printKNode(head, 1);
+    }
+
+    private static void printKNode(Node head, int k) {
+        if (head == null) {
+            System.out.println("null");
+            return;
+        }
+        Node fast = head;
+        int num = 0;
+        while (fast != null && num != k) {
+            fast = fast.next;
+            num++;
+        }
+
+        if (num != k) {
+            System.out.println("null");
+            return;
+        }
+
+        Node slow = head;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        System.out.println(slow);
+    }
+
+
+    static class Node {
+        int value;
+        Node next;
+
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" + "value=" + value + '}';
+        }
+    }
+}
---
+++ src/com/interview/Problem_16.java
@@ -0,0 +1,54 @@
+package com.interview;
+
+/**
+ * Created by weiqianghu on 17-7-8.
+ */
+public class Problem_16 {
+
+    public static void main(String[] args) {
+        Node head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, new Node(6, null))))));
+        printList(reverseList(head));
+        printList(reverseList(null));
+        printList(reverseList(new Node(1,null)));
+    }
+
+    private static void printList(Node head) {
+        Node p = head;
+        while (p != null) {
+            System.out.print(p);
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    private static Node reverseList(Node head) {
+        Node pre = null;
+        Node p = head;
+
+
+        while (p != null) {
+            Node next = p.next;
+            p.next = pre;
+            pre = p;
+            p = next;
+        }
+
+        return pre;
+    }
+
+
+    static class Node {
+        int value;
+        Node next;
+
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "["  + value + "]->";
+        }
+    }
+}
